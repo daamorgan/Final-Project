40a1310d1639cab30f6a8dcb5f0c8bdf0001fb30
johnnythekid <53437886+johnnythekid@users.noreply.github.com>
2019-11-17 14:10:53 -0500

    Add files via upload

---
+++ functions1.js
@@ -0,0 +1,21 @@
+window.onload=function(){
+    this.document.getElementById("click").onclick=function(){
+    var xhr=new XMLHttpRequest();
+    var url = "http://localhost:8080/issue.php"
+    xhr.onreadystatechange = doSomething; 
+    xhr.open('GET', url); 
+    xhr.send();
+    function doSomething() { 
+        if (xhr.readyState === XMLHttpRequest.DONE) { 
+            if (xhr.status === 200) { 
+                var response = xhr.responseText; 
+                 document.getElementById("result").innerHTML=response.trim(); 
+            }  else{
+                alert('There was a problem with the request.'); 
+    
+            }
+        }
+    }
+    }
+    }
+    
