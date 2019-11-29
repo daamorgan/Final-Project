e286e3b673792968871cc876ab295d3a8a82adf3
johnnythekid <53437886+johnnythekid@users.noreply.github.com>
2019-11-28 21:37:03 -0500

    updated database

--- schema.sql
+++ schema.sql
@@ -2,21 +2,22 @@ DROP DATABASE IF EXISTS ProjectFinal;
 CREATE DATABASE ProjectFinal;
 USE ProjectFinal;
 CREATE TABLE  Users(
- id INT  NOT NULL PRIMARY KEY,
+ id INT  NOT NULL AUTO_INCREMENT,
  firstname VARCHAR(32) DEFAULT NULL,
  lastname VARCHAR(32) DEFAULT NULL,
  password VARCHAR(16) DEFAULT NULL,
  email VARCHAR(32) DEFAULT NULL,
- date_joined VARCHAR(32) DEFAULT NULL
+ date_joined VARCHAR(32) DEFAULT NULL,
+ PRIMARY KEY(id)
 );
 INSERT INTO Users(id, firstname,lastname,password,email,date_joined)VALUES
-(1,"Ashley","Goldberg","password123","admin@bugme.com","2017-08-19"),
-(2,"Luke","Reynolds","Storminh23","Luke21@gmail.com","2015-11-13"),
-(3,"Dennis","RiechBach","Starr3yNight","admin@bugme.com","2013-12-12"),
-(4,"Lucy","Henry","Huntings12","admin@bugme.com","2016-07-07");
+("Ashley","Goldberg","password123","admin@bugme.com","2017-08-19"),
+("Luke","Reynolds","Storminh23","Luke21@gmail.com","2015-11-13"),
+("Dennis","RiechBach","Starr3yNight","admin@bugme.com","2013-12-12"),
+("Lucy","Henry","Huntings12","admin@bugme.com","2016-07-07");
 
 CREATE TABLE Issue(
- id INT  NOT NULL PRIMARY KEY,
+ id INT  NOT NULL AUTO_INCREMENT,
  title VARCHAR(32) DEFAULT NULL,
  description VARCHAR(32) DEFAULT NULL,
  type VARCHAR(16) DEFAULT NULL,
@@ -25,10 +26,11 @@ CREATE TABLE Issue(
  assigned_to VARCHAR(32) DEFAULT NULL,
  created_by VARCHAR(32) DEFAULT NULL,
  created VARCHAR(32) DEFAULT NULL,
- updated VARCHAR(32) DEFAULT NULL
+ updated VARCHAR(32) DEFAULT NULL,
+ PRIMARY KEY(id)
 );
 INSERT INTO Issue(id,title,description,type,priority,status,assigned_to,created_by,created,updated) VALUES
-(1,"My First Problem","stuff","bug",1,"open","1","2","2013-12-11","2012-03-09"),
-(2,"The Second problem","More Stuff","bug",2,"open","3","1","2013-10-03","2012-03-09"),
-(3,"I cant stop the information from leaking","My Site's security measures are so horrible","bug",5,"closed","4","1","2013-10-03","2012-03-09"),
-(4,"Constant CSRF attacks on my site","We have noticed that ","bug",3,"in pogress","3","2","2013-10-03","2012-03-09");
+("My First Problem","stuff","bug",1,"open","1","2","2013-12-11","2012-03-09"),
+("The Second problem","More Stuff","bug",2,"open","3","1","2013-10-03","2012-03-09"),
+("I cant stop the information from leaking","My Site's security measures are so horrible","bug",5,"closed","4","1","2013-10-03","2012-03-09"),
+("Constant CSRF attacks on my site","We have noticed that ","bug",3,"in pogress","3","2","2013-10-03","2012-03-09");
