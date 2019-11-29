ae5cbd75d7b3db5838e520ac3bf7cac505c88c4c
johnnythekid <53437886+johnnythekid@users.noreply.github.com>
2019-11-28 20:41:35 -0500

    updated database

--- schema.sql
+++ schema.sql
@@ -9,14 +9,11 @@ CREATE TABLE  Users(
  email VARCHAR(32) DEFAULT NULL,
  date_joined VARCHAR(32) DEFAULT NULL
 );
-INSERT INTO Users(id, firstname,lastname,password,email,date_joined)
-VALUES(1,"Ashley","Goldberg","password123","admin@bugme.com","2017-08-19");
-INSERT INTO Users(id, firstname,lastname,password,email,date_joined)
-VALUES(2,"Luke","Reynolds","Storminh23","Luke21@gmail.com","2015-11-13");
-INSERT INTO Users(id, firstname,lastname,password,email,date_joined)
-VALUES(3,"Dennis","RiechBach","Starr3yNight","admin@bugme.com","2013-12-12");
-INSERT INTO Users(id, firstname,lastname,password,email,date_joined)
-VALUES(4,"Lucy","Henry","Huntings12","admin@bugme.com","2016-07-07");
+INSERT INTO Users(id, firstname,lastname,password,email,date_joined)VALUES
+(1,"Ashley","Goldberg","password123","admin@bugme.com","2017-08-19"),
+(2,"Luke","Reynolds","Storminh23","Luke21@gmail.com","2015-11-13"),
+(3,"Dennis","RiechBach","Starr3yNight","admin@bugme.com","2013-12-12"),
+(4,"Lucy","Henry","Huntings12","admin@bugme.com","2016-07-07");
 
 CREATE TABLE Issue(
 id INT  NOT NULL PRIMARY KEY,
@@ -30,11 +27,8 @@ id INT  NOT NULL PRIMARY KEY,
   created VARCHAR(32) DEFAULT NULL,
  updated VARCHAR(32) DEFAULT NULL
 );
-INSERT INTO Issue(id,title,description,type,priority,status,assigned_to,created_by,created,updated)
-VALUES(1,"My First Problem","stuff","bug",1,"open","BJjOHNSON","LukeReynolds","2013-12-11","2012-03-09");
-INSERT INTO Issue(id,title,description,type,priority,status,assigned_to,created_by,created,updated)
-VALUES(2,"The Second problem","More Stuff","bug",2,"open","Nelson","Raymond","2013-10-03","2012-03-09");
-INSERT INTO Issue(id,title,description,type,priority,status,assigned_to,created_by,created,updated)
-VALUES(3,"I cant stop the information from leaking","My Site's security measures are so horrible","bug",5,"closed","David","Lewis Dreadfield","2013-10-03","2012-03-09");
-INSERT INTO Issue(id,title,description,type,priority,status,assigned_to,created_by,created,updated)
-VALUES(4,"Constant CSRF attacks on my site","We have noticed that ","bug",3,"in pogress","Nelson","Raymond","2013-10-03","2012-03-09");
+INSERT INTO Issue(id,title,description,type,priority,status,assigned_to,created_by,created,updated) VALUES
+(1,"My First Problem","stuff","bug",1,"open","BJjOHNSON","LukeReynolds","2013-12-11","2012-03-09"),
+(2,"The Second problem","More Stuff","bug",2,"open","Nelson","Raymond","2013-10-03","2012-03-09"),
+(3,"I cant stop the information from leaking","My Site's security measures are so horrible","bug",5,"closed","David","Lewis Dreadfield","2013-10-03","2012-03-09"),
+(4,"Constant CSRF attacks on my site","We have noticed that ","bug",3,"in pogress","Nelson","Raymond","2013-10-03","2012-03-09");
