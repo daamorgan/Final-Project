0c085aa88f07d92c866e1ce2acef492ddb45e90a
johnnythekid <53437886+johnnythekid@users.noreply.github.com>
2019-11-28 23:05:34 -0500

    Update Addtheuser.php

--- Addtheuser.php
+++ Addtheuser.php
@@ -5,7 +5,7 @@ $username = 'admin';
 $password = 'Bugme123';
 $dbname = 'ProjectFinal';
 if(preg_match("[a-zA-Z0-9]{8,}",$_GET["password"])){
-    $connect=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $conn=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $word=password_hash($_GET["password"],PASSWORD_DEFAULT);
     $stmt = $conn->query("INSERT INTO Users(id, firstname,lastname,password,email,date_joined)
     VALUES($_GET[firstname],$_GET[lastname],$word,$_GET[email],$the_day)");
