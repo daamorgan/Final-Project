02db871f95b2ee2a0cc6159d7bc1545a0970d843
johnnythekid <53437886+johnnythekid@users.noreply.github.com>
2019-11-29 13:11:02 -0500

    updated database

--- schema.sql
+++ schema.sql
@@ -10,16 +10,16 @@ CREATE TABLE  Users(
  date_joined VARCHAR(32) DEFAULT NULL,
  PRIMARY KEY(id)
 );
-INSERT INTO Users(id, firstname,lastname,password,email,date_joined)VALUES
+INSERT INTO Users(firstname,lastname,password,email,date_joined)VALUES
 ("Ashley","Goldberg","password123","admin@bugme.com","2017-08-19"),
 ("Luke","Reynolds","Storminh23","Luke21@gmail.com","2015-11-13"),
 ("Dennis","RiechBach","Starr3yNight","admin@bugme.com","2013-12-12"),
-("Lucy","Henry","Huntings12","admin@bugme.com","2016-07-07");
+("Lucy","Henry","Huntings12","admin2@bugme.com","2016-07-07");
 
 CREATE TABLE Issue(
  id INT  NOT NULL AUTO_INCREMENT,
- title VARCHAR(32) DEFAULT NULL,
- description VARCHAR(32) DEFAULT NULL,
+ title VARCHAR (255) DEFAULT NULL,
+ description VARCHAR(255) DEFAULT NULL,
  type VARCHAR(16) DEFAULT NULL,
  priority VARCHAR(32) DEFAULT NULL,
  status VARCHAR(32) DEFAULT NULL,
@@ -29,8 +29,9 @@ CREATE TABLE Issue(
  updated VARCHAR(32) DEFAULT NULL,
  PRIMARY KEY(id)
 );
-INSERT INTO Issue(id,title,description,type,priority,status,assigned_to,created_by,created,updated) VALUES
+INSERT INTO Issue(title,description,type,priority,status,assigned_to,created_by,created,updated) VALUES
 ("My First Problem","stuff","bug",1,"open","1","2","2013-12-11","2012-03-09"),
 ("The Second problem","More Stuff","bug",2,"open","3","1","2013-10-03","2012-03-09"),
 ("I cant stop the information from leaking","My Site's security measures are so horrible","bug",5,"closed","4","1","2013-10-03","2012-03-09"),
-("Constant CSRF attacks on my site","We have noticed that ","bug",3,"in pogress","3","2","2013-10-03","2012-03-09");
+("Constant CSRF attacks on my site","We have noticed that ","bug",3,"in progress","3","2","2013-10-03","2012-03-09"),
+("Access to my web application is being restricted","Admins and users alike can't login","Proposal",4,"in progress","4","2","2012-05-06","2010-04-04");
