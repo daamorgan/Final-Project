8f9936d0199b98783d7767b936244c6447bc980b
johnnythekid <53437886+johnnythekid@users.noreply.github.com>
2019-11-28 20:32:06 -0500

    updated php file

--- IssueHandler.php
+++ IssueHandler.php
@@ -6,12 +6,14 @@ $Type=$_POST["Type"];
 $Priority=$_POST["Priority"];
 $description=$_POST["description"];
 $the_day= Date('Y-m-d');
+$time=Date('H:i');
+$complete_time=$the_day.$time;
 $host = getenv('IP');
 $username = 'admin';
 $password = 'Bugme123';
 $dbname = 'ProjectFinal';
     $conn=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $checkLoginQuery = "INSERT INTO Issue(id,title,description,type,priority,status,assigned_to,created_by,created,updated)
-    VALUES(1,$text,,$Type,$Priority,open,$Assigned,$currentuser,$the_day,$the_day);";
+    VALUES(1,$text,$description,$Type,$Priority,open,$Assigned,$currentuser,$complete_time,$complete_time);";
     $stmt = $conn->query($checkLoginQuery);
 ?>
