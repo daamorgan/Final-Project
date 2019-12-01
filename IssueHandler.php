21ac69f52e10e4196c33baa5e6e30eaf4c90f026
johnnythekid <53437886+johnnythekid@users.noreply.github.com>
2019-11-30 20:56:25 -0500

    updated issuehandler.php

--- IssueHandler.php
+++ IssueHandler.php
@@ -2,6 +2,7 @@
 $currentuser=$_GET['username'];
 $text=$_POST["text"];
 $Assigned=$_POST["Assigned to"];
+$A_name=explode(" ",$Assigned);
 $Type=$_POST["Type"];
 $Priority=$_POST["Priority"];
 $description=$_POST["description"];
@@ -13,7 +14,11 @@ $username = 'admin';
 $password = 'Bugme123';
 $dbname = 'ProjectFinal';
     $conn=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $checkLoginQuery = "INSERT INTO Issue(title,description,type,priority,status,assigned_to,created_by,created,updated)
+    $firstquery="SELECT id FROM Users WHERE firstname= $A_name[0]";  
+    $secquery="SELECT id FROM Users WHERE email= $currentuser";
+    $stmt=$conn->query($firstquery);
+    $stmt2=$conn->query($secquery);
+    $Additionquery = "INSERT INTO Issue(title,description,type,priority,status,assigned_to,created_by,created,updated)
     VALUES($text,$description,$Type,$Priority,open,$Assigned,$currentuser,$complete_time,$complete_time);";
-    $stmt = $conn->query($checkLoginQuery);
+    $stmt3= $conn->query($checkLoginQuery);
 ?>
