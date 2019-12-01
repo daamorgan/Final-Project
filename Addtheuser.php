f503b4394d6fc0a9b776face76ab98e864276926
johnnythekid <53437886+johnnythekid@users.noreply.github.com>
2019-12-01 17:23:22 -0500

    updated way of adding the user

--- Addtheuser.php
+++ Addtheuser.php
@@ -4,11 +4,14 @@ $host = getenv('IP');
 $username = 'admin';
 $password = 'Bugme123';
 $dbname = 'ProjectFinal';
+$name1=filter_input(INPUT_GET,'firstname',FILTER_SANITIZE_SPECIAL_CHARS);
+$name2=filter_input(INPUT_GET,'lastname',FILTER_SANITIZE_SPECIAL_CHARS);
+$email=filter_input(INPUT_GET,'email',FILTER_SANITIZE_SPECIAL_CHARS);
 if(preg_match("[a-zA-Z0-9]{8,}",$_GET["password"])){
     $conn=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $word=password_hash($_GET["password"],PASSWORD_DEFAULT);
     $stmt = $conn->query("INSERT INTO Users(id, firstname,lastname,password,email,date_joined)
-    VALUES($_GET[firstname],$_GET[lastname],$word,$_GET[email],$the_day)");
+    VALUES($name1,$name2,$word,$email,$the_day)");
 }
 else
     echo "Password does not meet all necessary requirements"
