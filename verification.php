961918a94f8e41979c1e25641c4fde01f38657f9
daamorgan <55562274+daamorgan@users.noreply.github.com>
2019-12-01 17:36:09 -0500

    Update verification.php

--- verification.php
+++ verification.php
@@ -1,35 +1,30 @@
 <?php
 session_start();
-$user=filter_input(INPUT_GET,'user',FILTER_SANITIZE_SPECIAL_CHARS);
+$user=filter_input(INPUT_GET,'user',FILTER_SANITIZE_EMAIL);
 $pass=filter_input(INPUT_GET,'password',FILTER_SANITIZE_SPECIAL_CHARS);
-function verifyMe($name,$pass){
 $host = getenv('IP');
 $username = 'admin';
 $password = 'Bugme123';
 $dbname = 'ProjectFinal';
-    $conn=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $checkLoginQuery = "SELECT `id`, `firstname`, `lastname`,'email','password' FROM `Users` WHERE `email`='$user'";
-    $stmt = $conn->query($checkLoginQuery);
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if ($result && password_verify($pass,$result['password']))
-    {
-        $_SESSION["user_id"] = $result['id'];
-        $_SESSION["firstname"] = $result['firstname'];
-        $_SESSION["lastname"] = $result['lastname'];
-    }
-    else 
-    {
-        return false;
-    }
+$conn=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+$checkLoginQuery = "SELECT id, firstname, lastname, email, password FROM Users WHERE email='$user'";
+$stmt = $conn->query($checkLoginQuery);
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (!empty($result)){
+  foreach($result as $row)
+    if ($pass===$row['password']){
+      if ($user==="admin@bugme.com"){
+        $_SESSION["admin"]=true;
+      }else{
+          $_SESSION["admin"]=false;
+      }
+      $_SESSION["user_id"] = $row['id'];
+      $_SESSION["firstname"] = $row['firstname'];
+      $_SESSION["lastname"] = $row['lastname'];
+      header('location:homepage.html');
+    }else{
+      header('location:FirstPage.html');
+      echo "Invalid username or password";
+      
+  }
 }
-if(verifyMe($user,$pass)&& $user=="admin@bugme.com"){
-    header("Location:localhost:8080/homepage.html?username="+$user+"&Admin=true");
-    echo"You have been properly verified";
-}
-elseif(verifyMe($user,$pass)&& $user!="admin@bugme.com"){
-    header("Location:localhost:8080/homepage.html?username="+$user+"&Admin=false");
-}
-else{
-    echo "Attempt has failed please try again";
-}
-?>
