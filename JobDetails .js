a954b9dbaab69da0e5a51b2e91eb1210e04a96a6
johnnythekid <53437886+johnnythekid@users.noreply.github.com>
2019-11-30 20:18:13 -0500

    updated jobdetails.js

--- JobDetails .js
+++ JobDetails .js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     addJobDetails();
+    $("a").href+="?username"+GetParameterValues("username");
 });
 
 function addEvent(){
@@ -32,3 +33,12 @@ function addJobDetails(){
       alert('There was a problem with the request.');
     });
 }
+        function GetParameterValues(param) {  
+            var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');  
+            for (var i = 0; i < url.length; i++) {  
+                var urlparam = url[i].split('=');  
+                if (urlparam[0] == param) {  
+                    return urlparam[1];  
+                }  
+            }  
+        }  
