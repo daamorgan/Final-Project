55aea3a3e2c30115a05085c933923e54cf48a708
johnnythekid <53437886+johnnythekid@users.noreply.github.com>
2019-12-01 23:54:48 -0500

    updated databases

--- schema.sql
+++ schema.sql
@@ -11,10 +11,10 @@ CREATE TABLE  Users(
  PRIMARY KEY(id)
 );
 INSERT INTO Users(firstname,lastname,password,email,date_joined)VALUES
-("Ashley","Goldberg","Password123","admin@bugme.com","2017-08-19"),
-("Luke","Reynolds","Storminh23","Luke21@gmail.com","2015-11-13"),
-("Dennis","RiechBach","Starr3yNight","duncebat@bugs.com","2013-12-12"),
-("Lucy","Henry","Huntings12","mastermind@shutin.com","2016-07-07");
+("Ashley","Goldberg","Password123","admin@bugme.com","August 09,2013-8:12PM"),
+("Luke","Reynolds","Storminh23","Luke21@gmail.com","Novemeber 13,2012-4:16AM"),
+("Dennis","RiechBach","Starr3yNight","duncebat@bugs.com","December 12,2012-12:12PM"),
+("Lucy","Henry","Huntings12","mastermind@shutin.com","July 07,2016-09:12AM");
 
 CREATE TABLE Issue(
  id INT  NOT NULL AUTO_INCREMENT,
@@ -30,8 +30,8 @@ CREATE TABLE Issue(
  PRIMARY KEY(id)
 );
 INSERT INTO Issue(title,description,type,priority,status,assigned_to,created_by,created,updated) VALUES
-("My First Problem","stuff","bug",1,"open",1,2,"2013-12-11","2012-03-09"),
-("The Second problem","More Stuff","bug",2,"open",3,1,"2013-10-03","2012-03-09"),
-("I cant stop the information from leaking","My Site's security measures are so horrible","bug",5,"closed",4,1,"2013-10-03","2012-03-09"),
-("Constant CSRF attacks on my site","We have noticed that ","bug",3,"in progress",3,2,"2013-10-03","2012-03-09"),
-("Access to my web application is being restricted","Admins and users alike can't login","Proposal",4,"in progress",4,2,"2012-05-06","2010-04-04");
+("My First Problem","stuff","bug",major,"open",1,2,"November 13,2011-7:14PM","September 13,2012-4:32PM"),
+("The Second problem","More Stuff","bug",minor,"open",3,1,"January 22,2012-7:14PM","November 13,2012-7:56PM"),
+("I cant stop the information from leaking","My Site's security measures are so horrible","bug",Critical,"closed",4,1,"July 13,2012-7:14PM","June 26,2014-10:14PM"),
+("Constant CSRF attacks on my site","We have noticed that ","bug",Critical,"in progress",3,2,"Decemeber 05,2015-5:12AM","August 12,2018-9:14PM"),
+("Access to my web application is being restricted","Admins and users alike can't login","Proposal",major,"in progress",4,2,"November 13,2012-7:14PM","November 13,2012-7:14PM");
