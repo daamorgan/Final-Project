96c1bb3e7ce76e28260e6f409726df62edc1eb89
daamorgan <55562274+daamorgan@users.noreply.github.com>
2019-12-02 16:48:10 -0500

    Create CreateUserForm.php

---
+++ CreateUserForm.php
@@ -0,0 +1,18 @@
+
+  <h1>NEW USER</h1>
+    <div class="Area1">
+     <label for="Firstname">Firstname</label>
+     <input type="text" id="Firstname" required oninvalid="this.setCustomValidity('User ID is a must')">
+     <br>
+     <label for="Lastname">Lastname </label>
+     <input type="text" id="Lastname" required></div>
+     <br>
+      <label for="Password">Password</label>
+     <input type="password" id="Password"  minlength="8" required>
+     <br>
+     <label for="Email">Email</label>
+     <input type="email" id="Email" required>
+     <br>
+    <button onclick="createUser()" id="clicking">Submit</button>
+   </div>
+ </div>
