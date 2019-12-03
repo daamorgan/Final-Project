76cee19ab455f82c74ae0908e6c98be9b09a699e
daamorgan <55562274+daamorgan@users.noreply.github.com>
2019-12-03 14:12:34 -0500

    Update Addtheuser.php

--- Addtheuser.php
+++ Addtheuser.php
@@ -7,12 +7,15 @@ $dbname = 'ProjectFinal';
 $name1=filter_input(INPUT_GET,'firstname',FILTER_SANITIZE_SPECIAL_CHARS);
 $name2=filter_input(INPUT_GET,'lastname',FILTER_SANITIZE_SPECIAL_CHARS);
 $email=filter_input(INPUT_GET,'email',FILTER_SANITIZE_SPECIAL_CHARS);
-
+if(preg_match("[a-zA-Z0-9]{8,}",$_GET["password"])){
     $conn=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $word=password_hash($_GET["password"],PASSWORD_DEFAULT);
     $stmt = $conn->prepare("INSERT INTO Users(firstname,lastname,password,email,date_joined)
     VALUES(?,?,?,?,?)");
     $stmt->execute([$name1, $name2, $password, $email, $the_day]);
-
+    echo "User was successfully added";
+}
+else
+    echo "Failed to add user as password requirement are not met";
 
 ?>
