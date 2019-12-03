122faccbf4cd440bfca570bb2526e253f77c361e
daamorgan <55562274+daamorgan@users.noreply.github.com>
2019-12-03 14:26:37 -0500

    Update ProcessHomepage.php

--- ProcessHomepage.php
+++ ProcessHomepage.php
@@ -9,24 +9,21 @@
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     if ($_SERVER['REQUEST_METHOD']==='GET'){
       $filterOption =$_GET["filter"];
+      $id = $_SESSION['user_id'];
       if (!(empty($filterOption)) &&  isset($filterOption)) {
         if ($filterOption==="all"|| $filterOption==="startup"){
         	result("SELECT I.id, I.title, I.type, I.status, U.firstname ,U.lastname, I.created FROM Issue I Join Users U ON I.assigned_to=U.id",$conn);
         }elseif ($filterOption==="open"){
           result("SELECT I.id, I.title, I.type, I.status, U.firstname ,U.lastname, I.created FROM Issue I Join Users U ON I.assigned_to=U.id AND status='open';",$conn);  //need to fix
         }elseif ($filterOption==="mytickets"){
-        // 	$user= $_SESSION['firstname']." ". $_SESSION['lastname'];//MAY NEED TOp CHANGE THIS
-        // 	echo $user;
-        	$id = $_SESSION['user_id'];
         	result("SELECT * from Issue WHERE assigned_to=$id", $conn);
-          //result("SELECT I.id, I.title, I.type, I.status, U.firstname ,U.lastname, I.created FROM Issue I Join Users U ON I.assigned_to=U.id AND assigned_to='$id'" ,$conn);
         }else{
           $_SESSION['id']=$_GET['filter'];
         }
           
         }
     }
-}catch(PDOException $e) { 
+}catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage(); 
 }
 function result($querysql,$pdo){
@@ -47,7 +44,7 @@ function result($querysql,$pdo){
             <?php 
             $id=$row["id"];
             $title=$row["title"];
-            echo "<p>#$id <nav> <a class=issueTitle href=JobDetails.php id=$id>$title</span></a><nav>"; ?></td>
+            echo "<p>#$id <a class=issueTitle href=# onclick=JobDetails(event) id=$id> $title </a>"; ?> </td>
           <td><?= $row["type"];?></td>
           <td>
             <?php $status=strtoupper($row["status"]);
@@ -58,8 +55,11 @@ if ($status=="OPEN"){
 }else{
    echo "<p class=yellow>$status </p>";
 }?></td>
-          <td><?=$_SESSION["firstname"]." ".$_SESSION["lastname"];?></td>
-          <td><?php
+          <?php if ($_SESSION["admin"]){
+      ?> <td> <?=$_SESSION["firstname"]." ".$_SESSION["lastname"];?> </td> <?php
+    }else{
+  ?><td><?=$row["firstname"]." ".$row["lastname"];?></td><?php
+}
           $created=$row['created'];
           $arr=explode("-",$created);
           $date=$arr[0];
@@ -70,3 +70,9 @@ if ($status=="OPEN"){
         <?php endforeach; ?>
     </table><?php
 };
+
+if ($_SESSION["admin"]){
+  ?> <td> <?=$_SESSION["firstname"]." ".$_SESSION["lastname"];?> </td> <?php
+}else{
+  ?><td><?=$row["firstname"]." ".$row["lastname"];?></td><?php
+}
