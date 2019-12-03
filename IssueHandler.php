57c60cc99df81645b87b145ebd19abc33a6d6b60
daamorgan <55562274+daamorgan@users.noreply.github.com>
2019-12-03 14:19:33 -0500

    Update IssueHandler.php

--- IssueHandler.php
+++ IssueHandler.php
@@ -1,41 +1,23 @@
 <?php
-session_start();
+$currentuser=$_GET['username'];
+$text=$_POST["text"];
+$Assigned=$_POST["Assigned to"];
+$Type=$_POST["Type"];
+$Priority=$_POST["Priority"];
+$description=$_POST["description"];
+$the_day= Date('F j, Y');
+$time=Date('h:iA');
+$complete_time=$the_day." - ".$time;
 $host = getenv('IP');
 $username = 'admin';
 $password = 'Bugme123';
 $dbname = 'ProjectFinal';
-
 try{
     $conn=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $currentuser=$_SESSION['user_id'];
-        $Title = filter_input(INPUT_POST, 'Title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $Assigned = filter_input(INPUT_POST, 'Assigned To', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $Type = filter_input(INPUT_POST, 'Type', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $Description = filter_input(INPUT_POST, 'Description', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $Priority = filter_input(INPUT_POST, 'Priority', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
-        $A_name=explode(" ",$Assigned);
-        $the_day= Date('F j, Y');
-        $time=Date('h:iA');
-        $complete_time=$the_day." - ".$time;
-        $firstquery="SELECT id FROM Users WHERE firstname= '$A_name[0]'";  
-        $stmt=$conn->query($firstquery);
-        $results=$stmt->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($results as $detail){
-            $Assigned=$detail['id'];
-        }
-        
-        $Additionquery = "INSERT INTO Issue(title,description,type,priority,status,assigned_to,created_by,created,updated)
-            VALUES('$Title','$Description','$Type','$Priority','open',$Assigned,$currentuser,'$complete_time','$complete_time')";
-        $conn->query($Additionquery);
-
-        include_once 'CreateIssue.html';
-    }
+    $checkLoginQuery = "INSERT INTO Issue(title,description,type,priority,status,assigned_to,created_by,created,updated)
+    VALUES($text,$description,$Type,$Priority,open,$Assigned,$currentuser,$complete_time,$complete_time);";
+    $stmt = $conn->query($checkLoginQuery);
+    echo "Issue was succesfully added";
 }catch(PDOException $e) { 
-    echo "Connection failed: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage(); 
 }
-?>
-
-
-
